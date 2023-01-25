d0701ff8796629adc8ead06fb528a3ccb2dc52e2
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-01-25 08:20:12 -0600

    Add tool to Asset Menu

---
+++ Autotiler_SO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+#if (UNITY_EDITOR)
+
+[ExecuteInEditMode]
+[CreateAssetMenu(menuName = "AutoTiler_Menu_Name", fileName = "Default_Autotiler_File_Name")]
+
+public class Autotiler_SO : ScriptableObject
+{
+}
+
+#endif
